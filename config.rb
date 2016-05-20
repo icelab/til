9f0075bd4660046713fccda71ba9627de680f775
Max Wheeler <max@makenosound.com>
2016-05-20 15:21:40 +1000

    Add asset helpers.

--- config.rb
+++ config.rb
@@ -20,6 +20,8 @@ set :markdown,        fenced_code_blocks: true,
 
 require "lib/typography_helpers"
 helpers TypographyHelpers
+require "lib/asset_helpers"
+helpers AssetHelpers
 
 # Methods defined in the helpers block are available in templates
 # Add custom ones below
