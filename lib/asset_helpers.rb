898f347fbb74ac59aa0d2296245beeb6b25d566b
Max Wheeler <max@makenosound.com>
2016-05-20 22:24:55 +1000

    Make the asset_helper infer whether the asset_hash extensions is messing with filenames.

--- lib/asset_helpers.rb
+++ lib/asset_helpers.rb
@@ -1,6 +1,31 @@
 module AssetHelpers
   def render_asset(path)
-    path = path.gsub(/^\//, '')
-    sitemap.resources.find {|r| r.path == path }.render
+    # Remove leading slash
+    path = File.basename(path, '*')
+
+    # Infer whether `activate :asset_hash` is active matching against a hash
+    # with a format like foo-bar-b785915a.js
+    path_components = path.split("-")
+    asset_hash_active = if path_components.length == 0
+      false
+    elsif path_components.last.length === 8 && path_components.last =~ /(?:[0-9]+[a-z]|[a-z]+[0-9])/
+      true
+    else
+      false
+    end
+
+    if asset_hash_active != nil
+      path = components[0...-1].join("-")
+      sitemap.resources.find {|r|
+        # Remove the extension
+        File.basename(r.path, '*') == path
+      }.render
+    else
+      path = components.join("-")
+      sitemap.resources.find {|r|
+        # Remove the leading `/`
+        File.basename(r.path, '*') == path
+      }.render
+    end
   end
 end
