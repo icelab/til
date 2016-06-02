f70bfacf0b87ba1480d637d8c98ccdc5c2a60d89
Max Wheeler <max@makenosound.com>
2016-06-02 14:01:12 +1000

    Remove content from public-demo file.

--- source/assets/public-demo/index.js
+++ source/assets/public-demo/index.js
@@ -1,8 +1,3 @@
-var domready = require('domready')
-var viewloader = require('viewloader')
-
-var views = require('./views')
-
-domready(function () {
-  viewloader.execute(views)
-})
+/**
+ * Your JavaScript here
+ */
